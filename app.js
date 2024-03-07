91b1c9f6320db0a035f50a2b6476728e06b37d06
gabrielteixeiras <155987273+gabrielteixeiras@users.noreply.github.com>
2024-03-07 20:22:38 -0300

    Atualização no arquivo JS

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do número secreto";
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
-
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
-    
+    let chute = document.querySelector("input").value;
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTela = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertou!");
+        exibirTextoNaTela("p", mensagemTela);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p", "O número secreto é menor!");
+        } else {
+            exibirTextoNaTela("p", "O número secreto é maior!");
+        }
+    }
+    tentativas++;
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
